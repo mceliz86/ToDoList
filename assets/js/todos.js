454cfb507c58aeba8636c79f7ee62d28b08ee9db
mojeda86 <matias_goc@hotmail.com>
2019-06-03 11:43:36 -0300

    First commit

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+//Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to detele Todo
+$("ul").on("click", "span", function(e){
+	e.stopPropagation();
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+})
+
+//insert new todo when press enter
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		var todoText = $(this).val();
+		//create a new li and add to ul
+		$(this).val("");
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+todoText+"</li>");
+	}
+});
+
+//fadeout input type
+$(".fa-plus").on("click", function(){
+	$("input[type='text']").fadeToggle()
+});
